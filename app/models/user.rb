c3444907a66eafc65b42fd14371f4fae85d3ec72
Michael Jean <mjean@covermymeds.com>
2018-04-24 14:28:59 -0400

    Finished renaming from previous commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :lesson_activities
   has_many :lessons, through: :lesson_activities
   has_many :skill_activities
-  has_many :skills, through: :skill_activities
+  has_many :skills, through: :skill_progresses
   has_and_belongs_to_many :words
 end
