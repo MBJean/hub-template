537994feac8b40fcbc7cb497c2157d09c3f95394
Michael Jean <mjean@covermymeds.com>
2018-08-27 08:00:33 -0400

    Clean up of base components

--- app/javascript/components/Highlight.jsx
+++ app/javascript/components/Highlight.jsx
@@ -5,11 +5,15 @@ import createPostBody from '../helpers/createPostBody';
 import renderUnsafeXml from '../helpers/renderUnsafeXml';
 import fetchArray from '../helpers/fetchArray';
 import parseXml from '../helpers/parseXml';
+import Text from './Highlight/Text';
+import Entry from './Highlight/Entry';
 
 export default class Highlight extends Component {
 
   state = {
     comments_active: {},
+    is_active_content: false,
+    num_active: 0,
     translations_active: {}
   }
 
@@ -28,35 +32,37 @@ export default class Highlight extends Component {
 
   onClickHighlight = (options) => {
     let new_arr = [];
+    let new_num_active = this.state.num_active;
     switch (options.type) {
       case "comment":
         new_arr = {...this.state.comments_active};
-        new_arr[options.line_id].indexOf(options.entry_id) !== -1 ?
-          new_arr[options.line_id].splice(new_arr[options.line_id].indexOf(options.entry_id), 1):
-          new_arr[options.line_id].push(options.entry_id)
-        this.setState({
-          comments_active: new_arr
-        });
       break;
       case "translation":
         new_arr = {...this.state.translations_active};
-        new_arr[options.line_id].indexOf(options.entry_id) !== -1 ?
-          new_arr[options.line_id].splice(new_arr[options.line_id].indexOf(options.entry_id), 1):
-          new_arr[options.line_id].push(options.entry_id)
-        this.setState({
-          translations_active: new_arr
-        });
       break;
       default:
       break;
     }
+    if (new_arr[options.line_id].indexOf(options.entry_id) !== -1) {
+      new_arr[options.line_id].splice(new_arr[options.line_id].indexOf(options.entry_id), 1);
+      new_num_active--;
+    } else {
+      new_arr[options.line_id].push(options.entry_id);
+      new_num_active++;
+    }
+    this.setState({
+      is_active_content: new_num_active > 0 ? true: false,
+      num_active: new_num_active,
+      translations_active: new_arr
+    });
   }
 
   onMouseUpText = () => {
+    /*
     // use the getSelection API to capture user selection
       // checks against a selection of nothing
         // baseOffset represent index of beginning of selection, focusOffset represents final index of selection
-    document.getElementById("Highlight").onmouseup = function() {
+    document.getElementById("Highlight__lines").onmouseup = function() {
       let t = (document.all) ? document.selection.createRange().text : document.getSelection();
       if (t.baseOffset !== t.focusOffset) {
         console.log(t);
@@ -74,107 +80,43 @@ export default class Highlight extends Component {
         // render on page all highlighted text, keying comment to them in the rendered elements below
     // TODO: create editor feature
     // TODO: have editor feature appear on highlight
+    */
   }
 
   render() {
     return (
-      <div className="Highlight" onMouseUp={this.onMouseUpText}>
-        {
-          this.props.text.lines.map( (line) => {
-            return (
-              <div className="Highlight__line" key={line.id}>
-                <p className="Highlight__text">{line.text}
-                  {
-                    line.comments.map( comment => {
-                      return <span
-                              className="Highlight__highlight Highlight__highlight--comment"
-                              key={comment.id + comment.ind_start}
-                              onClick={() => { this.onClickHighlight({
-                                entry_id: comment.id,
-                                line_id: line.id,
-                                type: 'comment'
-                              }) }}
-                              style={
-                                {
-                                  top: 0,
-                                  left: (((comment.ind_start - 1) / line.text.length) * 100) + '%',
-                                  width: (((comment.ind_end - comment.ind_start) / line.text.length) * 100) + '%'
-                                }
-                              }></span>
-                    })
-                  }
-                  {
-                    line.translations.map( translation => {
-                      return <span
-                              className="Highlight__highlight Highlight__highlight--translation"
-                              key={translation.id + translation.ind_start}
-                              onClick={() => { this.onClickHighlight({
-                                entry_id: translation.id,
-                                line_id: line.id,
-                                type: 'translation'
-                              }) }}
-                              style={
-                                {
-                                  top: 0,
-                                  left: (((translation.ind_start - 1) / line.text.length) * 100) + '%',
-                                  width: (((translation.ind_end - translation.ind_start) / line.text.length) * 100) + '%'
-                                }
-                              }></span>
-                    })
-                  }
-                </p>
-                {
-                  line.comments.map( comment => {
-                    if (this.state.comments_active[line.id].indexOf(comment.id) !== -1) {
-                      return (
-                        <div className="Highlight__comment Highlight__comment--active" key={comment.id}>
-                          <span
-                            className="Highlight__close"
-                            onClick={() => { this.onClickHighlight({
-                              entry_id: comment.id,
-                              line_id: line.id,
-                              type: 'comment'
-                            }) }}>
-                            &#x2716;
-                          </span>
-                          {
-                            comment.entries.map( entry => {
-                              return <p className="Highlight__entry" key={entry.id}>{entry.text} &#8213; {entry.author}</p>;
-                            })
-                          }
-                        </div>
-                      );
-                    }
-                  } )
-                }
-                {
-                  line.translations.map( translation => {
-                    if (this.state.translations_active[line.id].indexOf(translation.id) !== -1) {
-                      return (
-                        <div className="Highlight__translation Highlight__translation--active" key={translation.id}>
-                          <span
-                            className="Highlight__close"
-                            onClick={() => { this.onClickHighlight({
-                              entry_id: translation.id,
-                              line_id: line.id,
-                              type: 'translation'
-                            }) }}>
-                            &#x2716;
-                          </span>
-                          {
-                            translation.entries.map( entry => {
-                              return <p className="Highlight__entry" key={entry.id}>&#8220;{entry.text}&#8221; &#8213; {entry.author}</p>;
-                            })
-                          }
-                        </div>
-                      );
-                    }
-                  } )
-                }
-              </div>
-            );
-          })
-        }
+      <div className="Highlight">
+        <div className="Highlight__lines" id="Highlight__lines" onMouseUp={this.onMouseUpText}>
+          {
+            this.props.text.lines.map( line => {
+              return (
+                <div className="Highlight__line" key={line.id}>
+                  <Text line={line} onClickHighlight={this.onClickHighlight}/>
+                </div>
+              );
+            })
+          }
+        </div>
+        <div className={`Highlight__content Highlight__content--${this.state.is_active_content ? 'active': 'inactive'}`}>
+          {
+            this.props.text.lines.map( line => {
+              return line.comments.map( comment => {
+                return this.state.comments_active[line.id].indexOf(comment.id) !== -1 ?
+                  <Entry entry={comment} line={line} onClickHighlight={this.onClickHighlight} type='comment'/>:
+                  null;
+              } )
+            } )
+          }
+          {
+            this.props.text.lines.map( line => {
+              return line.translations.map( translation => {
+                return this.state.translations_active[line.id].indexOf(translation.id) !== -1 ?
+                  <Entry entry={translation} line={line} onClickHighlight={this.onClickHighlight} type='translation'/>:
+                  null;
+              } )
+            } )
+          }
+        </div>
       </div>
     )
   }
