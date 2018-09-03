f44b908c5c531fdbf644a1bc93ef5d8e2066707f
Michael Jean <mjean@covermymeds.com>
2018-09-03 12:12:09 -0400

    Added basic views and routing for authors and texts

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,45 @@
+class AuthorsController < ApplicationController
+  def index
+    @authors = Author.all.sort
+  end
+
+  def by_short_name
+    @author = Author.find_by short_name: params[:short_name].titleize
+    @texts = @author.texts
+    render :by_short_name
+  end
+
+  def by_text
+    @author = Author.find_by short_name: params[:short_name].titleize
+    @text = @author.texts.find_by title: params[:text].titleize
+    @books = @text.books
+    render :by_text
+  end
+
+  def by_book
+    @author = Author.find_by short_name: params[:short_name].titleize
+    @text = @author.texts.find_by title: params[:text].titleize
+    @book = @text.books.find_by book_number: params[:book_number]
+    @sections = @book.sections
+    render :by_book
+  end
+
+  def by_section
+    @author = Author.find_by short_name: params[:short_name].titleize
+    @text = @author.texts.find_by title: params[:text].titleize
+    @book = @text.books.find_by book_number: params[:book_number]
+    @section = @book.sections.find_by identifier: params[:section_identifier]
+    @lines = @section.lines
+    render :by_section
+  end
+
+  def by_line
+    @author = Author.find_by short_name: params[:short_name].titleize
+    @text = @author.texts.find_by title: params[:text].titleize
+    @book = @text.books.find_by book_number: params[:book_number]
+    @section = @book.sections.find_by identifier: params[:section_identifier]
+    @line = @section.lines.find_by line_number: params[:line_number]
+    render :by_line
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "dashboard#index"
+  # routing for author and texts
+  get "authors" => "authors#index"
+  get "authors/:short_name" => "authors#by_short_name"
+  get "authors/:short_name/:text" => "authors#by_text"
+  get "authors/:short_name/:text/:book_number" => "authors#by_book"
+  get "authors/:short_name/:text/:book_number/:section_identifier" => "authors#by_section"
+  get "authors/:short_name/:text/:book_number/:section/:line_number" => "authors#by_line"
 
   get "users/profile" => "user#show", as: :user_profile
   post '/dictionary', to: 'dictionary#search', as: 'dictionary'
-  
+
 end
